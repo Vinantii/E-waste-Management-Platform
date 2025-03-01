3b04af0824ed102c7ceb21c87c6c5bb2154aac31
kart1k3y <kartikpatil039@gmail.com>
2025-03-01 23:42:35 +0530

    Minor changes in the frontend

--- app.js
+++ app.js
@@ -2414,7 +2414,7 @@ const updateUserPointsOnCompletion = wrapAsync(async (request) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   deleteExpiredEvents();
